c5e52b1b3573758afc313e8e27e7fbc3062f95b8
Roman Tupikov <romkacg@gmail.com>
2019-01-15 02:37:45 +0400

    git init

---
+++ src/main/java/com/github/hvits3rk/mailreceiver/MailreceiverApplication.java
@@ -0,0 +1,14 @@
+package com.github.hvits3rk.mailreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailreceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailreceiverApplication.class, args);
+    }
+
+}
+
