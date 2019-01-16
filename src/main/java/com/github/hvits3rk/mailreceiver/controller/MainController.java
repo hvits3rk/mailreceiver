644578cfd1c8e8e620fe55b4ec932008d08adc47
Roman Tupikov <romkacg@gmail.com>
2019-01-16 07:08:38 +0400

    Removed unnecessary classes

--- src/main/java/com/github/hvits3rk/mailreceiver/controller/MainController.java
+++ src/main/java/com/github/hvits3rk/mailreceiver/controller/MainController.java
@@ -1,47 +1,27 @@
 package com.github.hvits3rk.mailreceiver.controller;
 
-import com.github.hvits3rk.mailreceiver.entity.PriceItem;
-import com.github.hvits3rk.mailreceiver.repository.PriceItemRepository;
 import com.github.hvits3rk.mailreceiver.service.SparkService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.math.BigDecimal;
 
 @Controller
 public class MainController {
 
-    private final PriceItemRepository priceItemRepository;
     private final SparkService sparkService;
 
     @Value("${com.github.hvits3rk.debug.csvpath}")
     private String csvPath;
 
-    public MainController(PriceItemRepository priceItemRepository, SparkService sparkService) {
-        this.priceItemRepository = priceItemRepository;
+    public MainController(SparkService sparkService) {
         this.sparkService = sparkService;
     }
 
     @GetMapping("/")
     public String indexPage() {
-        PriceItem priceItem = new PriceItem();
-        priceItem.setCount(2);
-        priceItem.setDescription("Description");
-        priceItem.setNumber("number9");
-        priceItem.setPrice(BigDecimal.valueOf(134.25d));
-        priceItem.setSearchNumber("searchNumber9");
-        priceItem.setSearchVendor("searchVendor17");
-        priceItem.setVendor("vendor17");
-        priceItemRepository.save(priceItem);
-        return "index";
-    }
 
-    @GetMapping("/mail")
-    public String mailPage(Model model) {
-        return "mail";
+        return "index";
     }
 
     @RequestMapping("/debug/launchSpark")
--- src/main/java/com/github/hvits3rk/mailreceiver/repository/PriceItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.hvits3rk.mailreceiver.repository;
-
-import com.github.hvits3rk.mailreceiver.entity.PriceItem;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceItemRepository extends CrudRepository<PriceItem, Long> {
-}
